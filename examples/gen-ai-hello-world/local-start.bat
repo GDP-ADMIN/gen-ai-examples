f84d9e1ef8c79cb9f2c1166abf7569f3bcc8b41f
Hubert M. Sanyoto <hubert.m.sanyoto@gdplabs.id>
2025-01-09 13:20:35 +0700

    remove unnecessary declaration

--- examples/gen-ai-hello-world/local-start.bat
+++ examples/gen-ai-hello-world/local-start.bat
@@ -139,14 +139,8 @@ if "%cmd%"=="poetry" (
         call :handle_error "Failed to install %cmd% version %required_version%."
     )
     call :update_poetry_path
-    :: Wait for a moment to ensure the system recognizes the new installation
-    timeout /t 2 >nul
 ) else (
-    if "%cmd%"=="%PYTHON_CMD%" (
-        call :handle_error "Please use Python version %PYTHON_VERSION%."
-    ) else (
-        call :handle_error "Please install %cmd% version %required_version% or above."
-    )
+    call :handle_error "Please install %cmd% version %required_version% or above."
 )
 exit /b
 
