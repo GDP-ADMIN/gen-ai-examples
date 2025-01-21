197b9f86a1ecc323e7789fb29ea25a4901661805
HubertMSanyoto <hubert.m.sanyoto@gdplabs.id>
2025-01-21 14:05:27 +0700

    init example project

---
+++ examples/gen-ai-internal-hello-world/README.md
@@ -0,0 +1,155 @@
+# GenAI RAGO Example
+
+This is an example of how to use the gllm-pipeline library to build a simple RAG pipeline.
+
+> [!WARNING]
+> The GenAI SDK is currently only available for Linux, macOS, and Windows.
+
+## Prerequisites
+
+> [!NOTE]
+> You need to fulfill the prerequisites to run the script. They will be checked automatically when you execute it.
+
+1. **Python v3.12** (to run `python`).
+
+   - Using Python installer: 
+   
+      You can download the Python installer from the link [Python 3.12.8](https://www.python.org/downloads/release/python-3128/), select the version appropriate for your operating system, and run the installer. 
+      > [!NOTE]
+      > For Windows, please make sure to check the `Add python.exe to PATH` option during the installation process.
+      
+   - Using Conda:
+   
+      You can use [Miniconda](https://docs.anaconda.com/miniconda/install) to install and manage Python versions.
+
+## Running the code
+
+1. Configure environment variables: copy `.env.example` to `.env` and set up your `OPENAI_API_KEY` and `LANGUAGE_MODEL`.
+   - For Linux, macOS, or Windows WSL:
+
+      ```bash
+      cp .env.example .env
+      ```
+   
+   - For Windows Powershell or Command Prompt:
+   
+      ```powershell
+      copy .env.example .env
+      ```
+
+2. Execute the script: 
+   - For Linux, macOS, or Windows WSL:
+
+      ```bash
+      ./local-start.sh
+      ```
+      > [!NOTE]
+      > On Windows, you can either install [WSL (Windows Subsystem for Linux)](https://learn.microsoft.com/en-us/windows/wsl/install) or execute the batch file in Windows Powershell or Command Prompt as described in the next section.
+
+   - For Windows Command Prompt:
+      
+      ```cmd
+      local-start.bat
+      ```
+
+   - For Windows Powershell:
+      
+      ```powershell
+      .\local-start.bat
+      ```
+
+The program will then wait for your question:
+
+> Question:
+
+Type your question and press enter. For example, you can try asking:
+
+```
+What are the documents?
+```
+
+The RAG pipeline will return the following response (more or less):
+
+> _The documents mentioned are referred to as Mock document 1, Mock document 2, and Mock document 3. However, without additional context or content from these documents, I cannot provide specific details about their contents or purposes._
+
+## FAQs
+
+### 1. I got error `Unable to access repository GDP-ADMIN/gen-ai-internal. Please check your GitHub credentials and repository permissions.`. How do I fix it?
+
+You need to add your SSH key to the Github account. Please follow this [link to add SSH keys to your account](https://docs.github.com/en/authentication/connecting-to-github-with-ssh/adding-a-new-ssh-key-to-your-github-account).
+
+
+### 2. I got error `Error executing component StuffResponseSynthesizer__user_query_response_synthesis_bundle`. How do I fix it?
+
+Check `.env` file (if you already run `local-start.sh`, it is auto generated). You might have provided invalid `OPENAI_API_KEY` and/or `LANGUAGE_MODEL`.
+
+```txt
+# GDP Labs Gen AI Hello World Example
+# Please edit the following variables with your own values.
+
+OPENAI_API_KEY =<YOUR_OPENAI_API_KEY> # Get your OpenAI API key from https://platform.openai.com/api-keys
+LANGUAGE_MODEL =<VALID_OPENAI_LANGUAGE_MODEL_NAME> # e.g. "gpt-3.5-turbo", "gpt-4o-mini", "gpt-4o"
+```
+
+### 3. I got error `OSError: could not get source code`. How do I fix it?
+
+This error is currently expected. If you can see the response, it means the pipeline is working as expected.
+
+Here is an example of the error.
+
+```
+Question: who are you?
+/home/resti/glair/gen-ai/testing/gen-ai-examples/examples/gen-ai-hello-world/.venv/lib/python3.11/site-packages/gllm_core/schema/component.py:150: RuntimeWarning: Failed to analyze the _run method: could not get source code.
+Traceback (most recent call last):
+  File "/home/resti/glair/gen-ai/testing/gen-ai-examples/examples/gen-ai-hello-world/.venv/lib/python3.11/site-packages/gllm_core/schema/component.py", line 168, in _analyze_run_method
+  File "/home/resti/anaconda3/envs/gen-ai-internal-test/lib/python3.11/inspect.py", line 1270, in getsource
+    lines, lnum = getsourcelines(object)
+                  ^^^^^^^^^^^^^^^^^^^^^^
+  File "/home/resti/anaconda3/envs/gen-ai-internal-test/lib/python3.11/inspect.py", line 1252, in getsourcelines
+    lines, lnum = findsource(object)
+                  ^^^^^^^^^^^^^^^^^^
+  File "/home/resti/anaconda3/envs/gen-ai-internal-test/lib/python3.11/inspect.py", line 1081, in findsource
+    raise OSError('could not get source code')
+OSError: could not get source code
+
+Response:
+I am an AI assistant here to help you with your questions and provide information. How can I assist you today?
+```
+
+### 4. I got error `ImportError: failed to find libmagic`. How do I fix it?
+
+This typically happens if you use macOS or Windows.
+
+#### Install `libmagic` in macOS
+
+For macOS, there two alternatives:
+
+1. If you have [conda](https://docs.anaconda.com/miniconda/install/) installed:
+
+```
+conda install libmagic
+cd /usr/local/lib
+sudo ln -s /opt/miniconda3/pkgs/libmagic-5.39-h6ba3021_1/lib/libmagic.dylib libmagic.dylib
+```
+
+> [!WARNING]
+> Please adjust the path of your Conda installation if it differs. In the example above, the path is `/opt/miniconda3/`.
+
+2. If you have [brew](https://brew.sh/) installed:
+
+```
+brew install libmagic
+```
+
+### 5. I got error `Unable to find installation candidates`. How do I fix it?
+
+Our binary SDK can only be run on specific OSes:
+
+1. **Linux / WSL**: x86_64 architecture.
+
+2. **macOS**: x86_64 (Intel) or arm64 (Apple Silicon) architecture.
+
+   1. macOS version 13 or newer for x86_64 (Intel).
+   2. macOS version 14 or newer for arm64 (Apple Silicon).
+
+3. **Windows**: 64bit architecture.
---
+++ examples/gen-ai-internal-hello-world/local-start.bat
@@ -0,0 +1,290 @@
+@echo off
+setlocal EnableDelayedExpansion
+
+:: Set Variables
+set "MIN_PYTHON_VERSION=3.11 3.12"
+set "MIN_POETRY_VERSION=1.8.1"
+set "MIN_GCLOUD_VERSION=493.0.0"
+
+set "PYTHON_CMD=python"
+set PYTHON_PATH=""
+set POETRY_PATH=""
+set COMMAND_VERSION=
+
+(
+:: Main Section
+call :log "Checking gen-ai-hello-world example requirements..."
+call :get_python_path
+call :check_requirements
+call :install_command "poetry" "%MIN_POETRY_VERSION%"
+call :check_github_access "GDP-ADMIN/gen-ai-internal"
+call :deactivate_conda
+call :log "All requirements are satisfied."
+call :log "Setting up gen-ai-hello-world example..."
+call :copy_env_file
+call :configure_poetry_python_path
+call :install_dependencies
+call :log "gen-ai-hello-world example ready to run."
+call :log "Running gen-ai-hello-world example..."
+!POETRY_PATH! run python gen_ai_hello_world/main.py || (
+    call :handle_error "Failed to run the application"
+    exit /b 1
+)
+call :log "gen-ai-hello-world example finished running."
+exit /b
+)
+
+
+:get_python_path
+where python >nul 2>&1
+if %errorlevel% equ 0 ( 
+    set "PYTHON_CMD=python"
+) else (
+    call :log "python command not found, trying to use python3 instead..."
+    where python3 >nul 2>&1
+    if %errorlevel% equ 0 (
+        set "PYTHON_CMD=python3"
+    ) else (
+        call :handle_error "Python not found. Please install Python with one of these versions %MIN_PYTHON_VERSION%"
+    )
+)
+for /f "delims=" %%i in ('where %PYTHON_CMD%') do (
+    set "PYTHON_PATH=%%i"
+    goto :python_break
+)
+:python_break
+call :log "use python command: !PYTHON_CMD!"
+call :log "PYTHON_PATH will be set to: !PYTHON_PATH!"
+exit /b
+
+
+:check_requirements
+call :log "Checking system requirements..."
+:: Check command version
+call :check_command_version "%PYTHON_CMD%" "%MIN_PYTHON_VERSION%" "--version"
+call :log "System requirements are satisfied."
+exit /b
+
+
+:: Check command version function
+:check_command_version
+set "cmd=%~1"
+set "required_version=%~2"
+set "version_flag=%~3"
+where !cmd! >nul 2>&1
+if %errorlevel% neq 0 (
+    call :log "!cmd! is not installed. Required version: !required_version!."
+    call :install_command !cmd! !required_version!
+    exit /b 1
+)
+call :get_command_version !cmd! !version_flag!
+:: Check for exact Python version
+if "!cmd!"=="%PYTHON_CMD%" (
+    call :check_version_array !COMMAND_VERSION! !cmd! "%MIN_PYTHON_VERSION%"
+    if !errorlevel! neq 0 exit /b 1
+) else (
+    call :compare_versions "!COMMAND_VERSION!" "!required_version!"
+    if !errorlevel! equ 0 (
+        call :log "!cmd! version !required_version! or above is required. You have version !COMMAND_VERSION!."
+        call :install_command !cmd! !required_version!
+        exit /b 1
+    )
+)
+call :log "!cmd! is installed and meets the required version (!required_version!). Current version: !COMMAND_VERSION!"
+exit /b
+
+
+:check_version_array
+set "current_version=%~1"
+set "command_name=%~2"
+set "version_array=%~3"
+set "version_matched=false"
+for %%v in (!version_array!) do (
+    powershell -Command "$current='%current_version%'; if($current -match '^%%v\.[0-9]+$') { exit 0 } else { exit 1 }" >nul 2>&1
+    if !errorlevel! equ 0 (
+        set "version_matched=true"
+        goto :version_match_end
+    )
+)
+:version_match_end
+if "!version_matched!"=="false" (
+    call :handle_error "!command_name! version must be one of (!version_array!). Current version: %current_version%."
+    exit /b 1
+)
+exit /b 0
+
+
+:compare_versions
+:: Parameters: %~1 = current version, %~2 = required version
+:: Returns: errorlevel 0 if current version is less than required version
+::         errorlevel 1 if current version is greater than or equal to required version
+powershell -Command "function Compare-Version { param($ver1, $ver2); $v1 = [version]::new($ver1); $v2 = [version]::new($ver2); if ($v1 -lt $v2) { exit 0 } else { exit 1 } }; Compare-Version '%~1' '%~2'" >nul 2>&1
+exit /b %errorlevel%
+
+
+:install_command
+set cmd=%~1
+set required_version=%~2
+if "%cmd%"=="poetry" (
+    call :log "Attempting to install the latest version of %cmd%..."
+    :: Install the latest version of Poetry
+    call :log "Installing Poetry via powershell..."
+        powershell -Command "(Invoke-WebRequest -Uri https://install.python-poetry.org -UseBasicParsing).Content | & $env:PYTHON_CMD -" || (
+        call :handle_error "Failed to install %cmd% version %required_version%."
+    )
+    call :update_poetry_path
+) else (
+    call :handle_error "Please install %cmd% version %required_version% or above."
+)
+exit /b
+
+
+:update_poetry_path
+set "POETRY_PATH=%APPDATA%\pypoetry\venv\Scripts\poetry.exe"
+
+if not exist "!POETRY_PATH!" (
+    call :get_command_version "%PYTHON_CMD%" "--version"
+    :: Search for the folder matching the command version, ignoring the random part
+    set "COMMAND_VERSION=!COMMAND_VERSION:~0,4!"
+    for /d %%F in (%LOCALAPPDATA%\Packages\PythonSoftwareFoundation.Python.!COMMAND_VERSION!_*) do (
+        set "POETRY_PATH=%%F\LocalCache\Roaming\pypoetry\venv\Scripts\poetry.exe"
+        goto :path_break
+    )
+    :path_break
+    if not exist "!POETRY_PATH!" (
+        call :handle_error "POETRY_PATH is not found. Please ensure Poetry is installed correctly."
+        exit /b 1
+    )
+)
+call :log "POETRY_PATH will be set to: !POETRY_PATH!"
+exit /b
+
+
+:: Check GitHub access function
+:check_github_access
+setlocal
+set "repo=%~1"
+echo Checking access to GitHub repository: %repo%...
+:: Check if git is installed
+where git >nul 2>nul
+if errorlevel 1 (
+    call :handle_error "Git is not installed. Please install git first."
+    exit /b 1
+)
+:: Try to do a test clone to check access
+git ls-remote "https://github.com/%repo%" HEAD >nul 2>nul
+if errorlevel 1 (
+    call :handle_error "Unable to access repository %repo%. Please check your GitHub credentials and repository permissions."
+    exit /b 1
+)
+call :log "Successfully verified access to %repo%."
+endlocal
+exit /b
+
+
+:: Deactivate conda function
+:deactivate_conda
+where conda >nul 2>&1
+if %errorlevel% equ 0 (
+    for /f "tokens=* usebackq" %%i in (`conda info --base`) do (
+        set "CONDA_BASE=%%i"
+    )
+    
+    if defined CONDA_DEFAULT_ENV (
+        call :log "Conda environment '%CONDA_DEFAULT_ENV%' is active. Deactivating..."
+        call conda deactivate
+    )
+    call :log "All Conda environments have been deactivated."
+) else (
+    call :log "Conda is not installed. Skipping conda deactivation."
+)
+exit /b
+
+
+:get_command_version
+:: Parameters: %~1 = command, %~2 = version flag
+set cmd=%~1
+set version_flag=%~2
+set version_output=
+:: Execute the command and capture the output
+for /f "tokens=* usebackq" %%i in (`!cmd! !%version_flag! 2^>^&1`) do (
+    set "version_output=%%i"   
+    :: Extract version number using a regular expression
+    for /f "tokens=* usebackq" %%v in (`powershell -Command "$version='!version_output!'; if($version -match '\d+\.\d+\.\d+') { $matches[0] }"`) do (
+        set "COMMAND_VERSION=%%v"
+    )
+    goto :break
+)
+:break
+if "!COMMAND_VERSION!"=="" (
+    call :handle_error "Could not determine !cmd! version. Please ensure it is installed correctly."
+    exit /b 1
+) 
+exit /b 0
+
+
+:copy_env_file
+if not exist ".env" (
+    copy .env.example .env >nul 2>&1
+    call :log "Successfully copied '.env.example' to '.env'."
+    call :log "Please change the values in the .env file with your own values and then run './local-start.sh' again."
+    call :exit_application
+)
+call :log ".env file exists. Continuing..."
+exit /b
+
+
+:configure_poetry_python_path
+call :log "Configuring Poetry to use Python !PYTHON_PATH!..."
+!POETRY_PATH! env use !PYTHON_PATH! || call :handle_error "Failed to configure Poetry to use Python !PYTHON_PATH!."
+exit /b
+
+
+:install_dependencies
+call :log "Installing dependencies..."
+!POETRY_PATH! add python-magic-bin  || call :handle_error "Failed to add python-magic-bin."
+!POETRY_PATH! install || call :handle_error "Failed to install dependencies."
+exit /b
+
+
+:: Logging function
+:log
+for /f "tokens=2 delims==" %%I in ('wmic os get localdatetime /value') do set datetime=%%I
+echo [%datetime:~0,4%-%datetime:~4,2%-%datetime:~6,2% %datetime:~8,2%:%datetime:~10,2%:%datetime:~12,2%] %~1
+exit /b
+
+
+:: Error handling function
+:handle_error
+call :log "An error occurred: %~1"
+call :exit_application
+exit /b
+
+
+:: Exit application function
+:exit_application
+call :log "Exiting..."
+call :exit_batch
+
+
+:exit_batch
+if not exist "%temp%\ExitBatchYes.txt" call :build_yes
+call :CtrlC <"%temp%\ExitBatchYes.txt" 1>nul 2>&1
+
+
+:: Error code terminated by user
+:CtrlC
+cmd /c exit -1073741510
+
+
+:: To give yes reponse when exit
+:build_yes
+pushd "%temp%"
+set "yes="
+copy nul ExitBatchYes.txt >nul
+for /f "delims=(/ tokens=2" %%Y in (
+  '"copy /-y nul ExitBatchYes.txt <nul"'
+) do if not defined yes set "yes=%%Y"
+echo %yes%>ExitBatchYes.txt
+popd
+exit /b
