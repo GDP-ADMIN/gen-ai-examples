e8a88de7d1d568f03dea393d5a4df92e0f049046
timotiusnc <timotius.nc@gmail.com>
2025-05-08 15:06:10 +0700

    Use `general-purpose` chatbot instead of `no-op`.

--- examples/custom-tool-and-agent/mcp_tools/glchat_tools_stdio.py
+++ examples/custom-tool-and-agent/mcp_tools/glchat_tools_stdio.py
@@ -18,8 +18,7 @@ def message(prompt: str) -> int:
     """
     response = requests.post(
         "https://chat-api.gdplabs.id/message",
-        data={'chatbot_id': 'no-op', 'message': prompt, 'content-type': 'application/json'},
-        stream=True
+        data={'chatbot_id': 'general-purpose', 'message': prompt, 'content-type': 'application/json'}
     )
     
     final_message = ""
