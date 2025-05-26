7d875aa2107224d0d9967e1884f3e70d6e5ae938
Christian Chen <christian.t.s.l.chen@gdplabs.id>
2025-05-26 22:13:41 +0700

    remove unused

--- examples/aip-agent-quickstart/aip_agent_quickstart/agents/weather_agent/weather_agent/__init__.py
+++ examples/aip-agent-quickstart/aip_agent_quickstart/agents/weather_agent/weather_agent/__init__.py
@@ -4,7 +4,6 @@ Authors:
     Christian Trisno Sen Long Chen (christian.t.s.l.chen@gdplabs.id)
 """
 
-from .config import SERVER_AGENT_NAME
 from .tools import weather_tool
 
-__all__ = ["SERVER_AGENT_NAME", "weather_tool"]
+__all__ = ["weather_tool"]
