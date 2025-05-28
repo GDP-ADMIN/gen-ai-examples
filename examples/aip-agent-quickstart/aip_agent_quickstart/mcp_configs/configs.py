ef79cd519cac7ae83ab85fd184a0ee7589eb32f8
Raymond Christopher <raymond.christopher@gdplabs.id>
2025-05-29 00:16:11 +0700

    MCP done

--- examples/aip-agent-quickstart/aip_agent_quickstart/mcp_configs/configs.py
+++ examples/aip-agent-quickstart/aip_agent_quickstart/mcp_configs/configs.py
@@ -15,7 +15,7 @@ mcp_config_stdio = {
 
 mcp_config_arxiv_sse = {
     "arxiv_tools": {
-        "url": "http://localhost:8006/sse",
+        "url": "https://mcp.obrol.id/f/sse",
         "transport": "sse",
     }
 }
