2f96212c933ebd5a42c51d18fc2a552d6beeba22
Christian Trisno Sen Long Chen <christian.t.s.l.chen@gdplabs.id>
2025-05-31 00:13:12 +0700

    [Agent] Hello World A2A LangChain Agent (#57)
    * add langchain examples
    
    * add hello world
    
    * change to binary 0.2.12
    
    * fix agent binary version
    
    * change tool import

---
+++ examples/aip-agent-quickstart/hello_world_a2a_langchain_client.py
@@ -0,0 +1,53 @@
+"""Example of a General Assistant LangChainAgent that can delegate tasks to specialized agents.
+
+This example demonstrates:
+1. Configuring A2A settings for a client agent.
+2. Creating a general assistant agent that can help with various queries.
+3. Delegating specific tasks to specialized agents via A2A.
+4. Handling responses and providing relevant advice.
+
+Authors:
+    Putu Ravindra Wiguna (putu.r.wiguna@gdplabs.id)
+"""
+
+import json
+
+from langchain_openai import ChatOpenAI
+
+from gllm_agents.agent.langchain_agent import LangChainAgent
+from gllm_agents.agent.types import A2AClientConfig
+from gllm_agents.utils.logger_manager import LoggerManager
+
+logger = LoggerManager().get_logger(__name__)
+
+
+def main():
+    """Main function demonstrating the General Assistant agent with A2A capabilities."""
+    # Init Assistant Agent
+    llm = ChatOpenAI(model="gpt-4.1", temperature=0)
+
+    # Create and return the LangChainAgent
+    assistant_agent = LangChainAgent(
+        name="AssistantAgentLangChain",
+        instruction="""You are a helpful assistant that can help with various tasks
+        by delegating to specialized agents.""",
+        llm=llm,
+        tools=[],
+    )
+
+    # Discover agents
+    client_a2a_config = A2AClientConfig(
+        discovery_urls=["http://localhost:8001"],
+    )
+    agent_cards = assistant_agent.discover_agents(client_a2a_config)
+
+    query = "What is the weather in Jakarta?"
+    logger.info(f"Processing Query: {query}")
+
+    response = assistant_agent.send_to_agent(agent_cards[0], query)
+    logger.info(f"Raw response: \n\n{json.dumps(response, indent=4)}")
+    logger.info(f"Agent Response: {response.get('content', str(response))}")
+
+
+if __name__ == "__main__":
+    main()
